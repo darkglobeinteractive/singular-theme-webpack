cbb317dadbddcd0a07564ef823dce14c488115d2
Robbie Schneider <darkglobeinteractive@gmail.com>
2024-11-27 12:24:23 -0500

    Getting theme to this point.

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,45 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+  entry: {
+    public: [
+      './src/scss/public.scss',
+      './block-patterns/js/block-patterns.js',
+      './blocks/blocks.js',
+      './src/js/global.js'
+    ],
+    admin: [
+      './src/scss/admin.scss',
+      './src/js/acf-api.js'
+    ]
   },
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: '[name].min.js',
+    clean: true
+  },
+  devtool: "source-map",
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      },
       {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { 
+            loader: 'css-loader',
+            options: {
+              url: false
+            }
+          },
+          { 
+            loader: 'sass-loader'
+          }
+        ]
       }
     ]
   },
