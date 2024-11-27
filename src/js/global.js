de24d32f6847788c848ac2426458d3f7020fba6e
Robbie Schneider <darkglobeinteractive@gmail.com>
2024-11-27 13:45:42 -0500

    Rewriting/extracting the throttling function from the custom function to avoid issue with webpack compilation

--- src/js/global.js
+++ src/js/global.js
@@ -1,25 +1,21 @@
-/*
- * jQuery throttle / debounce - v1.1 - 3/7/2010
- * http://benalman.com/projects/jquery-throttle-debounce-plugin/
- * 
- * Copyright (c) 2010 "Cowboy" Ben Alman
- * Dual licensed under the MIT and GPL licenses.
- * http://benalman.com/about/license/
- */
-(function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
-
-/* Throttle example
-$(window).on('resize', $.throttle(500, function() {
-    position_menu_bar();
-  })
-);
-*/
-
 jQuery(document).ready(function($) {
+
+  /*
+  jQuery throttle / debounce - v1.1 - 3/7/2010 -- Rewritten to accommodate webpack compilation
+  http://benalman.com/projects/jquery-throttle-debounce-plugin/
+  */
+  function st_throttle(e,f,j,i){var c=undefined;var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g}
+
+  /*
+  $(window).on('scroll', st_throttle(500, function() {
+    console.log('do something');
+  }));
+  */
+
 
   /* HEADER ON SCROLL ------------------------------------------- */
   /* NOTE: If header dimensions change, use a fixed header, not a sticky header
-  $(window).on('scroll', $.throttle(500, function() {
+  $(window).on('scroll', st_throttle(500, function() {
 
     var window_scroll_top = $(window).scrollTop();
 
