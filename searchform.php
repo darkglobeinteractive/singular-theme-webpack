54e5736365164e907acc97e5f2d3a61ff84e1445
Robbie Schneider <darkglobeinteractive@gmail.com>
2025-04-11 15:32:43 -0400

    Adding dynamic ID generation for the search text field in case it appears multiple times on a page

--- searchform.php
+++ searchform.php
@@ -1,7 +1,8 @@
+<?php $input_id = singular_random_id( 10, 'sid-' ); ?>
 <form role="search" method="get" class="search-form" action="/">
 	<div class="search-form-wrap">
-		<label class="screen-reader-text" for="s">Search for:</label>
-		<input id="s" name="s" type="text" value="<?php echo ( get_search_query() ? get_search_query() : '' ); ?>" />
+		<label for="<?php echo $input_id; ?>">Search for:</label>
+		<input id="<?php echo $input_id; ?>" name="s" type="text" value="<?php echo ( get_search_query() ? get_search_query() : '' ); ?>" />
 		<button type="submit" class="search-submit">Submit</button>
 	</div>
 </form>
