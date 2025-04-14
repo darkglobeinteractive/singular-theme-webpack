1113c9a9ded51945996632348ff0d92232bfde25
Robbie Schneider <darkglobeinteractive@gmail.com>
2025-04-14 14:36:35 -0400

    Adding a play button SVG icon

---
+++ svg/play-button.php
@@ -0,0 +1,9 @@
+<span class="svg-icon play-button">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 124 124" role="img" aria-label="Play Button Icon">
+    <title>Play Button Icon</title>
+    <g>
+      <path class="circle" d="M66.7,957.4a62,62,0,1,0,62,62A61.917,61.917,0,0,0,66.7,957.4Z" transform="translate(-4.7 -957.4)"/>
+    </g>
+    <path class="path" d="M36.7,28.7,88.382,59.709,36.7,90.719Z" transform="translate(5.762 2.241)"/>
+  </svg>
+</span>
