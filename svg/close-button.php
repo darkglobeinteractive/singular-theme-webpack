f557cd56d0b486bff0cbd5596118fde559bd913e
Robbie Schneider <darkglobeinteractive@gmail.com>
2025-04-16 14:08:28 -0400

    New close button for modals and other stuff

---
+++ svg/close-button.php
@@ -0,0 +1,10 @@
+<span class="svg-icon close-button">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" role="img" aria-label="Close Button">
+    <title>Close Button</title>
+    <circle class="circle" cx="24" cy="24" r="24"/>
+    <g transform="translate(-1248.394 -1408.394)">
+      <line class="path" x2="30" transform="translate(1261.394 1421.394) rotate(45)"/>
+      <line class="path" x2="30" transform="translate(1261.394 1442.606) rotate(-45)"/>
+    </g>
+  </svg>
+</span>
