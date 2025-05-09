8f80723bbde567d7bc2ccfb0582a57650595d130
Robbie Schneider <darkglobeinteractive@gmail.com>
2025-05-09 13:20:00 -0400

    Adding some more animations

--- src/js/animations.js
+++ src/js/animations.js
@@ -46,6 +46,50 @@ jQuery(document).ready(function($) {
     }
   });
 
+  // SLIDE RIGHT: Slide-in from the right
+  gsap.registerEffect({
+    name: 'slideUp',
+    effect: (targets, config) => {
+      return gsap.fromTo(
+        targets,
+        {
+          y: '50px'
+        },
+        {
+          scrollTrigger: {
+            trigger: targets,
+            start: 'top 95%'
+          },
+          y: 'auto',
+          duration: 1,
+          ease: 'power2.out'
+        }
+      )
+    }
+  });
+
+  gsap.registerEffect({
+    name: 'staggerFade',
+    effect: (targets, config) => {
+      return gsap.fromTo(
+        targets,
+        {
+          opacity: '0'
+        },
+        {
+          scrollTrigger: {
+            trigger: targets,
+            start: 'top 90%'
+          },
+          opacity: '1',
+          duration: 1,
+          stagger: 0.5,
+          ease: 'power2.out'
+        }
+      )
+    }
+  });
+
 
   /* DEFINE CSS SELECTORS --------------------------------------- */
 
@@ -55,6 +99,14 @@ jQuery(document).ready(function($) {
 
   $('.st-animate.slide-right').each(function() {
     gsap.effects.slideRight($(this));
+  });
+
+  $('.st-animate.slide-up').each(function() {
+    gsap.effects.slideUp($(this));
+  });
+
+  $('.st-animate.stagger-fade .item').each(function(i, e) {
+    gsap.effects.staggerFade(e);
   });
 
 });
