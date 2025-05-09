6262d8357f233c587a92546f560e58690c6cc06a
Robbie Schneider <darkglobeinteractive@gmail.com>
2025-05-09 13:19:20 -0400

    Revising the Rich Banner to a more simplified version because this is what we usually use.

--- templates/banner-rich.php
+++ templates/banner-rich.php
@@ -7,46 +7,30 @@ $classes_array = array( 'banner-rich', 'has-black-background-color', 'has-backgr
 $styles_array = array();
 $btn_array = array( 'btn' );
 
-// Handle banner and button classes based-on type of banner, defaulting to 'full'
-$rich_banner_type = ( get_field( 'rich_banner_type', $gv['qid'] ) ?: 'full' );
-
 // Set banner mask to 'false' by default
 $rich_banner_mask = false;
 
-// Full width banners have background images on the banner element and transparent buttons
-if ( $rich_banner_type == 'full' ) {
+// Determine the full background type, defaulting to a static image
+$rich_banner_background_type = ( get_field( 'rich_banner_background_type', $gv['qid'] ) ?: 'static-image' );
 
-  // Add full class to array
-  $classes_array[] = 'full';
+// Add full background type to classes array
+$classes_array[] = $rich_banner_background_type;
 
-  // Determine the full background type, defaulting to a static image
-  $rich_banner_background_type = ( get_field( 'rich_banner_background_type', $gv['qid'] ) ?: 'static-image' );
+// Check for background video files and placement
+if ( $rich_banner_background_type == 'video' ) {
 
-  // Add full background type to classes array
-  $classes_array[] = $rich_banner_background_type;
+  // Check for videos
+  $rich_banner_videos = ( have_rows( 'rich_banner_videos', $gv['qid'] ) ? true : false );
 
-  // Check for background video files and placement
-  if ( $rich_banner_background_type == 'video' ) {
-
-    // Check for videos
-    $rich_banner_videos = ( have_rows( 'rich_banner_videos', $gv['qid'] ) ? true : false );
-
-    // Set video background position
-    $rich_banner_videos_horizontal = ( get_field( 'rich_banner_videos_horizontal', $gv['qid'] ) ?: 'horizontal-center' );
-    $rich_banner_videos_vertical = ( get_field( 'rich_banner_videos_vertical', $gv['qid'] ) ?: 'vertical-center' );
-    $rich_banner_videos_background_position = $rich_banner_videos_horizontal.' '.$rich_banner_videos_vertical;
-
-  }
-
-  // Set banner mask
-  $rich_banner_mask = ( get_field( 'rich_banner_mask', $gv['qid'] ) == 'Yes' ? true : false );
-
-} else {
-
-  // Other wise it's an 'image-aligned' banner and either 'image-left' or 'image-right'
-  array_push( $classes_array, 'image-aligned', $rich_banner_type );
+  // Set video background position
+  $rich_banner_videos_horizontal = ( get_field( 'rich_banner_videos_horizontal', $gv['qid'] ) ?: 'horizontal-center' );
+  $rich_banner_videos_vertical = ( get_field( 'rich_banner_videos_vertical', $gv['qid'] ) ?: 'vertical-center' );
+  $rich_banner_videos_background_position = $rich_banner_videos_horizontal.' '.$rich_banner_videos_vertical;
 
 }
+
+// Set banner mask
+$rich_banner_mask = ( get_field( 'rich_banner_mask', $gv['qid'] ) == 'Yes' ? true : false );
 
 // Handle banner image
 $rich_banner_image = ( get_field( 'rich_banner_image', $gv['qid'] ) ?: false );
@@ -69,9 +53,9 @@ $btn_classes = implode( ' ', $btn_array );
 $rich_banner_text = ( get_field( 'rich_banner_text', $gv['qid'] ) ?: false );
 $rich_banner_button = ( get_field( 'rich_banner_button', $gv['qid'] ) ? singular_assemble_link( get_field( 'rich_banner_button' ), $btn_classes ) : false );
 ?>
-<div id="banner-rich"<?php echo $block_classes; ?><?php echo ( $rich_banner_type == 'full' ? $block_styles : '' ); ?>>
+<div id="banner-rich"<?php echo $block_classes; ?><?php echo $block_styles; ?>>
   <div class="wrap">
-    <div class="content<?php if ( $rich_banner_type != 'full' ): ?> has-black-background-color<?php endif; ?>">
+    <div class="content">
       <div class="content-wrap">
         <?php echo $rich_banner_text; ?>
         <?php if ( $rich_banner_button ): ?>
@@ -81,11 +65,8 @@ $rich_banner_button = ( get_field( 'rich_banner_button', $gv['qid'] ) ? singular
         <?php endif; ?>
       </div>
     </div>
-    <?php if ( $rich_banner_type != 'full' ): ?>
-      <div class="image"<?php echo $block_styles; ?>></div>
-    <?php endif; ?>
   </div>
-  <?php if ( $rich_banner_type == 'full' && $rich_banner_background_type == 'video' && $rich_banner_videos ): ?>
+  <?php if ( $rich_banner_background_type == 'video' && $rich_banner_videos ): ?>
     <video class="video-bg <?php echo $rich_banner_videos_background_position; ?>" autoplay loop muted width="100%">
       <?php while ( have_rows( 'rich_banner_videos', $gv['qid'] ) ): the_row(); ?>
         <?php 
