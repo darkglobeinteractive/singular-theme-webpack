df605a2bf243a0a125f7647e3e4d0369bcdacad3
Robbie Schneider <darkglobeinteractive@gmail.com>
2026-02-25 13:02:16 -0500

    Linkable Background: Modifying block to use core WordPress responsive image in-place of background image.

--- blocks/linkable-image-background/linkable-image-background.php
+++ blocks/linkable-image-background/linkable-image-background.php
@@ -3,11 +3,11 @@
 $classes_array = array( 'linkable-image-background' );
 
 // Handle the image
-$image_src = ( get_field( 'lib_image' ) ? get_field( 'lib_image' )['sizes']['large'] : get_template_directory_uri().'/img/placeholder-image.png' );
+$image_id = ( get_field( 'lib_image' ) ?: get_field( 'default_image_background', 'option' ) );
 $image_position = ( get_field( 'lib_image_position' ) ?: 'center center' );
-$image_styles_array[] = 'background-image: url(\''.$image_src.'\')';
-$image_styles_array[] = 'background-position: '.$image_position;
-$image_styles = ' style="'.implode( '; ', $image_styles_array ).'"';
+$image_styles_array = array( 'object-position: '.$image_position );
+$image_styles = implode( '; ', $image_styles_array );
+$image_object = wp_get_attachment_image( $image_id, 'large', false, array( 'class' => 'image', 'fetchpriority' => 'low', 'style' => $image_styles ) );
 
 // Handle link variables
 $link_check = ( get_field( 'lib_link_check' ) == 'none' ? false : get_field( 'lib_link_check' ) );
@@ -42,24 +42,27 @@ $block_classes = singular_attribute_builder( $classes_array, 'class' );
 
 <div<?php echo $block_classes; ?>>
   <?php if ( !$link_check || is_admin() ): ?>
-    <span class="image"<?php echo $image_styles; ?>>
+    <span class="image-wrap">
       <?php if ( $play_button ): ?>
         <span class="admin-play-button"></span>
       <?php endif; ?>
+      <?php echo $image_object; ?>
     </span>
   <?php elseif ( $link_check == 'modal' ): ?>
-    <a href="<?php echo $href; ?>" class="image modaal-video"<?php echo $image_styles; ?>>
+    <a href="<?php echo $href; ?>" class="image-wrap modaal-video">
       <span class="visually-hidden">Launch Modal Window</span>
       <?php if ( $play_button ): ?>
         <?php include( locate_template( 'svg/play-button.php', false, false ) ); ?>       
       <?php endif; ?>
+      <?php echo $image_object; ?>
     </a>
   <?php elseif ( $link_check == 'standard' ): ?>
-    <a href="<?php echo $href; ?>" class="image"<?php echo $image_styles; ?><?php echo $target; ?>>
+    <a href="<?php echo $href; ?>" class="image-wrap"<?php echo $target; ?>>
       <span class="visually-hidden">Goto Page</span>
       <?php if ( $play_button ): ?>
         <?php include( locate_template( 'svg/play-button.php', false, false ) ); ?>       
       <?php endif; ?>
+      <?php echo $image_object; ?>
     </a>
   <?php endif; ?>
 </div>
