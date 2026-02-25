49db20681051ffec03e2d44731ae8351119382cc
Robbie Schneider <darkglobeinteractive@gmail.com>
2026-02-25 12:30:03 -0500

    Posts: Modifying post item to use core WordPress responsive image in-place of background image.

--- templates/post-item.php
+++ templates/post-item.php
@@ -1,27 +1,23 @@
 <?php
-$permalink = get_permalink();
-if ( has_post_thumbnail() ) {
-  $post_thumbnail = get_the_post_thumbnail_url( get_the_ID(), 'medium_large' );
-  $post_thumbnail_id = get_post_thumbnail_id();
-  $post_thumbnail_alt = ( get_post_meta ( $post_thumbnail_id, '_wp_attachment_image_alt', true ) ?: 'Thumbnail image for: '.get_the_title() );
-} else {
-  $post_thumbnail = get_template_directory_uri().'/img/default-blog-thumbnail.jpg';
-  $post_thumbnail_alt = 'Thumbnail image for: '.get_the_title();
-}
+$pid = get_the_ID(); 
+$permalink = get_permalink( $pid );
 
-$blurb = ( has_excerpt() ? get_the_excerpt() : substr( get_the_excerpt(), 0, 250 ).'[...]' );
+$blurb = ( has_excerpt( $pid ) ? get_the_excerpt( $pid ) : substr( get_the_excerpt( $pid ), 0, 250 ).'[...]' );
 ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'item' ); ?>>
+<article id="post-<?php echo $pid; ?>" <?php post_class( 'item' ); ?>>
 
   <?php if ( !is_admin() ): ?>
-    <?php edit_post_link( 'Edit', '<div class="edit-link">', '</div>', get_the_ID() ); ?>
+    <?php edit_post_link( 'Edit', '<div class="edit-link">', '</div>', $pid ); ?>
   <?php endif; ?>
 
   <div class="thumbnail">
     <?php if ( !is_admin() ): ?>
       <a href="<?php echo $permalink; ?>">
     <?php endif; ?>
-    <img src="<?php echo $post_thumbnail; ?>" alt="<?php echo $post_thumbnail_alt; ?>" />
+
+    <?php $thumbnail_array = array( 'class' => 'image', 'fetchpriority' => 'low' ); ?>
+    <?php echo ( has_post_thumbnail( $pid ) ? get_the_post_thumbnail( $pid, 'medium_large', $thumbnail_array ) : wp_get_attachment_image( get_field( 'default_image_post', 'option' ), 'medium_large', false, $thumbnail_array ) ); ?>
+
     <?php if ( !is_admin() ): ?>
       </a>
     <?php endif; ?>
@@ -33,36 +29,36 @@ $blurb = ( has_excerpt() ? get_the_excerpt() : substr( get_the_excerpt(), 0, 250
       <?php if ( !is_admin() ): ?>
         <a href="<?php echo $permalink; ?>">
       <?php endif; ?>
-      <?php echo get_the_title(); ?>
+      <?php echo get_the_title( $pid ); ?>
       <?php if ( !is_admin() ): ?>
         </a>
       <?php endif; ?>
     </h2>
 
     <?php
-    $categories = get_the_category();
-    if (!empty($categories)) {
+    $categories = get_the_category( $pid );
+    if ( !empty( $categories ) ) {
       echo '<div class="blog-taxonomy categories"><strong>Categories:</strong> ';
-      foreach($categories as $category) {
-        echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a>';
+      foreach( $categories as $category ) {
+        echo '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '">' . esc_html( $category->name ) . '</a>';
       }
       echo '</div>';
     }
     ?>
 
     <?php
-    $tags = get_the_tags();
-    if (!empty($tags)) {
+    $tags = get_the_tags( $pid );
+    if ( !empty( $tags ) ) {
       echo '<div class="blog-taxonomy tags"><strong>Tags:</strong> ';
-      foreach($tags as $tag) {
-        echo '<a href="' . esc_url(get_category_link($tag->term_id)) . '">' . esc_html($tag->name) . '</a>';
+      foreach( $tags as $tag ) {
+        echo '<a href="' . esc_url( get_category_link( $tag->term_id ) ) . '">' . esc_html( $tag->name ) . '</a>';
       }
       echo '</div>';
     }
     ?>
 
     <div class="blurb">
-      <?php echo get_the_excerpt(); ?>
+      <?php echo $blurb; ?>
     </div>
 
     <div class="btn-wrap">
