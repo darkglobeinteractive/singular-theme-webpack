12f77e7222d5c52b3ad8951cb44520f27491fd1f
darkglobeinteractive <darkglobeinteractive@gmail.com>
2026-04-21 11:24:35 -0400

    Animations: Converting animations to use native JavaScript and fixing a few bugs

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function($) {
+document.addEventListener('DOMContentLoaded', function() {
 
   /* REGISTER EFFECTS ------------------------------------------- */
 
@@ -61,7 +61,7 @@ jQuery(document).ready(function($) {
     }
   });
 
-  // SLIDE RIGHT: Slide-in from the right
+  // SLIDE UP: Slide-in from below
   gsap.registerEffect({
     name: 'slideUp',
     effect: (targets, config) => {
@@ -158,7 +158,7 @@ jQuery(document).ready(function($) {
 
 
   /* DIRECT IMMEDIATE ANIMATIONS -------------------------------- */
-  if ($('.im-animate.fade-in').length > 0) {
+  if (document.querySelectorAll('.im-animate.fade-in').length > 0) {
     gsap.fromTo('.im-animate.fade-in', {
       opacity: 0,
     },{
@@ -168,7 +168,7 @@ jQuery(document).ready(function($) {
     });
   }
 
-  if($('.im-animate.stagger-fade').length > 0) {
+  if (document.querySelectorAll('.im-animate.stagger-fade').length > 0) {
     gsap.fromTo('.im-animate.stagger-fade', {
       opacity: 0
     },{
@@ -181,80 +181,81 @@ jQuery(document).ready(function($) {
 
 
   /* DEFINE CSS SELECTORS --------------------------------------- */
-  $('.st-animate.fade-in').each(function() {
-    gsap.effects.fadeIn($(this));
+  document.querySelectorAll('.st-animate.fade-in').forEach(function(el) {
+    gsap.effects.fadeIn(el);
   });
 
-  $('.st-animate.slide-right').each(function() {
-    gsap.effects.slideRight($(this));
+  document.querySelectorAll('.st-animate.slide-right').forEach(function(el) {
+    gsap.effects.slideRight(el);
   });
 
-  $('.st-animate.slide-up').each(function() {
-    gsap.effects.slideUp($(this));
+  document.querySelectorAll('.st-animate.slide-up').forEach(function(el) {
+    gsap.effects.slideUp(el);
   });
 
 
   /* DEFINE GLOBAL/STANDARD BLOCKS AND SITUATIONS --------------- */
 
+  // Generate an array of classes to omit from the global .wp-block-singular animation below
+  const blockExclusions = ['.accordion-container', '.color-block-cta-columns', '.image-cta-columns', '.testimonial-block'];
+
   // global basic content blocks
-  $('.wp-block-singular').not('.accordion-container,.color-block-cta-columns,.image-cta-columns,.testimonial-block').each(function() {
-    $('> .wp-block-group__inner-container', this).each(function() {
-      gsap.effects.fadeIn($(this));
-      gsap.effects.slideUp($(this));
+  document.querySelectorAll(`.wp-block-singular:not(${blockExclusions.join(', ')})`).forEach(function(el) {
+    el.querySelectorAll(':scope > .wp-block-group__inner-container').forEach(function(inner) {
+      gsap.effects.fadeIn(inner);
+      gsap.effects.slideUp(inner);
     });
   });
-  
+
   // accordions
-  $('.wp-block-singular.accordion-container').each(function() {
-    gsap.effects.staggerFade('.wp-block-singular.accordion-container .accordion-item', { duration: 0.5, stagger: 0.2 });
-  });
-  
-  // article items
-  $('article.item').each(function() {
-    gsap.effects.staggerFade('article.item');
+  document.querySelectorAll('.wp-block-singular.accordion-container').forEach(function(el) {
+    gsap.effects.staggerFade(el.querySelectorAll('.accordion-item'), { duration: 0.5, stagger: 0.2 });
   });
 
+  // article items
+  gsap.effects.staggerFade(document.querySelectorAll('article.item'));
+
   // footer
-  $('#footer').each(function() {
-    gsap.effects.staggerFade('#footer > .wrap');
+  document.querySelectorAll('#footer').forEach(function(el) {
+    gsap.effects.staggerFade(el.querySelectorAll(':scope > .wrap'));
   });
 
   // rich banner
-  $('.banner-rich').each(function() {
-    gsap.effects.staggerFadeSlide('.banner-rich .content-wrap > *');
+  document.querySelectorAll('.banner-rich').forEach(function(el) {
+    gsap.effects.staggerFadeSlide(el.querySelectorAll('.content-wrap > *'));
   });
 
   // search results
-  $('#search-results').each(function() {
-    gsap.effects.staggerFade('#search-results .search-result');
+  document.querySelectorAll('#search-results').forEach(function(el) {
+    gsap.effects.staggerFade(el.querySelectorAll('.search-result'));
   });
 
   // simple banner
-  $('.banner').each(function() {
-    gsap.effects.staggerFadeSlide('.banner .content > *');
+  document.querySelectorAll('.banner').forEach(function(el) {
+    gsap.effects.staggerFadeSlide(el.querySelectorAll('.content > *'));
   });
 
 
   /* DEFINE CUSTOM/UNIQUE BLOCKS AND SITUATIONS ----------------- */
 
   // color block cta columns
-  $('.wp-block-singular.color-block-cta-columns').each(function() {
-    gsap.effects.staggerFade('.wp-block-singular.color-block-cta-columns .wp-block-columns .wp-block-column');
-  });  
+  document.querySelectorAll('.wp-block-singular.color-block-cta-columns').forEach(function(el) {
+    gsap.effects.staggerFade(el.querySelectorAll('.wp-block-columns .wp-block-column'));
+  });
 
   // image cta columns
-  $('.wp-block-singular.image-cta-columns').each(function() {
-    gsap.effects.staggerFade('.wp-block-singular.image-cta-columns .wp-block-columns .wp-block-column');
-  });  
+  document.querySelectorAll('.wp-block-singular.image-cta-columns').forEach(function(el) {
+    gsap.effects.staggerFade(el.querySelectorAll('.wp-block-columns .wp-block-column'));
+  });
 
   // team members grid
-  $('.team-members-block').each(function() {
-    gsap.effects.staggerFade('.team-members-block .team-member');
+  document.querySelectorAll('.team-members-block').forEach(function(el) {
+    gsap.effects.staggerFade(el.querySelectorAll('.team-member'));
   });
 
   // testimonials block
-  $('.wp-block-singular.testimonial-block').each(function() {
-    gsap.effects.staggerFadeSlide('.testimonial-section', this);
+  document.querySelectorAll('.wp-block-singular.testimonial-block').forEach(function(el) {
+    gsap.effects.staggerFadeSlide(el.querySelectorAll('.testimonial-section'));
   });
 
 });
